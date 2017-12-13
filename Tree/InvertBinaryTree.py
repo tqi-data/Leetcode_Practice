3e8ee201302671a00a1734c7b2e82939b9407516
Tingting, Qi <32271867+tqi-data@users.noreply.github.com>
2017-12-13 17:03:44 -0500

    Add files via upload

---
+++ Tree/InvertBinaryTree.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+"""
+Invert a binary tree.
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+
+to
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+"""
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+        
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
+
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right = TreeNode(7)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(9)
+
+a = Solution()
+newroot = a.invertTree(root)
+
+print newroot.val
+print newroot.left.left.val
