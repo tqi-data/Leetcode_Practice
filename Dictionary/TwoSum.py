7cf93701d248993f302c8828559db4075cce0e52
Tingting, Qi <32271867+tqi-data@users.noreply.github.com>
2017-12-14 10:59:47 -0500

    Add files via upload

---
+++ Dictionary/TwoSum.py
@@ -0,0 +1,31 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = dict()
+        for i in range(len(nums)):
+            key = target - nums[i]
+            # if the difference is in the dictionary
+            if key in d:
+                return [d[key] , i]
+            # if the difference is NOT in the dictionary
+            # put a new entry in the dictionary, key=number, value=position
+            else:
+                d[nums[i]] = i
+                
+        return 
+        
+
+nums = [3, 2, 4]
+target = 7
+
+a = Solution()
+print "The indices of two numbers adding up to", target, "is", a.twoSum(nums, target)
+
+
